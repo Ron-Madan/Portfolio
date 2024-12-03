5f1dd1f661ff650bb864ddba9704c9914601a43e
Ron Madan <37419003+Ron-Madan@users.noreply.github.com>
2024-12-02 19:14:58 -0500

    Update compare function with error check

--- mastermind-game/src/gameLogic/guess.jsx
+++ mastermind-game/src/gameLogic/guess.jsx
@@ -6,9 +6,11 @@ export default class Guess {
   compare(solution) {
     let correctColors = 0;
     let correctPosition = 0;
+    let incorrectColors = 0;
   
     // First, check for correct positions
     this.sequence.forEach((color, i) => {
+      if (color === null) { return; }
       if (color === solution[i]) {
         correctPosition++;
       }
@@ -16,10 +18,22 @@ export default class Guess {
   
     // Now, check for correct colors in wrong positions
     this.sequence.forEach((color, i) => {
+      if (color === null) { return; }
       if (color !== solution[i] && solution.includes(color)) {
         correctColors++;
       }
     });
+
+    // Confirm check logic is correct
+    this.sequence.forEach((color, i) => {
+      if (!solution.includes(color)) {
+        incorrectColors++;
+      }
+    });
+    if (incorrectColors !== 5 - correctColors - correctPosition) {
+      console.log('Error: Incorrect colors count is wrong');
+      return -1;
+    }
   
     return [correctPosition, correctColors];  // Return feedback
   }
